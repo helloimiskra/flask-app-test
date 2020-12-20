e1f2b6b8e840e222e148e761b5b84fdcf0e63e3d
helloimiskra <helloimiskra@gmail.com>
2020-12-20 23:19:07 +0900

    adds delete method and updates form

--- app.py
+++ app.py
@@ -19,19 +19,6 @@ class BlogPost(db.Model):
     def __repr__(self):
         return "Blog post " + str(self.id)
 
-# all_posts = [
-#     {
-#         "title": "Post 1",
-#         "content": "This is the content of post numero uno!",
-#         "author": "Iskra"
-#     },
-#     {
-#         "title": "Post 2",
-#         "content": "This is the content of post numero dos!"
-#     }
-# ]
-
-
 
 @app.route('/')
 def index():
@@ -42,7 +29,8 @@ def posts():
     if request.method=="POST":
         post_title = request.form["title"]
         post_content = request.form["content"]
-        new_post = BlogPost(title=post_title, content=post_content, author='Iskra')
+        post_author = request.form["author"]
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
         db.session.add(new_post)
         db.session.commit()
         return redirect('/posts')
@@ -50,13 +38,23 @@ def posts():
         all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
         return render_template('posts.html', posts=all_posts)
 
-@app.route('/home/<string:name>')
-def hello(name):
-    return "Hello, " + name + '.'
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
 
-@app.route('/onlyget', methods=["GET"]) #allows you to limit methods for routes
-def get_req():
-    return 'You can only get this webpage.'
+    
+
+
+# @app.route('/home/<string:name>')
+# def hello(name):
+#     return "Hello, " + name + '.'
+
+# @app.route('/onlyget', methods=["GET"]) #allows you to limit methods for routes
+# def get_req():
+#     return 'You can only get this webpage.'
 
 if __name__ == "__main__":
     app.run(debug=True)
