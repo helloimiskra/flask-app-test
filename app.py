12888cf80abf2bf5211ebe539b228e60b8b45fe3
helloimiskra <helloimiskra@gmail.com>
2020-12-20 18:27:09 +0900

    creates database and static folders

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
 
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False) #nullable means field is required
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(30), nullable=False, default="N/A") #strings need character limit
+    date_posted = db.Column(db.DateTime, nullable=False, default= datetime.utcnow) #remember to import datetime to use this
+
+    def __repr__(self):
+        return "Blog post " + str(self.id)
 
 all_posts = [
     {
         "title": "Post 1",
-        "content": "This is the content of post numero uno!"
+        "content": "This is the content of post numero uno!",
+        "author": "Iskra"
     },
     {
         "title": "Post 2",
