ddd99c4785e980f415c86d44fb4e974082ab2b71
helloimiskra <helloimiskra@gmail.com>
2020-12-20 17:56:27 +0900

    adds base html and basic post html

--- app.py
+++ app.py
@@ -2,13 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/home/<string:name>')
+
+all_posts = [
+    {
+        "title": "Post 1",
+        "content": "This is the content of post numero uno!"
+    },
+    {
+        "title": "Post 2",
+        "content": "This is the content of post numero dos!"
+    }
+]
+
+
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
 
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=all_posts)
+
+@app.route('/home/<string:name>')
 def hello(name):
     return "Hello, " + name + '.'
 
