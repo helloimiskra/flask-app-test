471974fa4059260296882aaa54386e3d1ea367e4
helloimiskra <helloimiskra@gmail.com>
2020-12-20 17:41:03 +0900

    creates templates folder

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/home/<string:name>')
+
 @app.route('/')
 
-def hello():
-    return "Hello World"
+def index():
+    return render_template('index.html')
+
+def hello(name):
+    return "Hello, " + name + '.'
+
+@app.route('/onlyget', methods=["GET"]) #allows you to limit methods for routes
+def get_req():
+    return 'You can only get this webpage.'
 
 if __name__ == "__main__":
     app.run(debug=True)
+
